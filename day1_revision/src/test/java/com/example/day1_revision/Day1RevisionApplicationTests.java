a4e7faac6b10bc51b448db64643327ac4b1449fc
Arpityadav09 <arpit.yadav_cs23@gla.ac.in>
2026-01-19 15:20:52 +0530

    Add files via upload

---
+++ day1_revision/src/test/java/com/example/day1_revision/Day1RevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day1_revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1RevisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
